27efaa1433ddb1b4dd21598739335508bb03cf93
zhihuai1982 <zhihuai1982@zhihuai1982deiMac.local>
2020-08-19 00:20:20 +0800

    fix

--- install_requiredPackages.R
+++ install_requiredPackages.R
@@ -1,28 +1,29 @@
 #install scater,scran,Rstne
-source("https://bioconductor.org/biocLite.R")
-biocLite("scater")
-biocLite("scran")
-biocLite("Rtsne")
-biocLite("biomaRt")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("scater")
+BiocManager::install("scran")
+BiocManager::install("Rtsne")
+BiocManager::install("biomaRt")
 
 #scimpute
 if(library(devtools,logical.return=T)){
   install_github("Vivianstats/scImpute",force=TRUE)
 }else{
   warning("please install devtools and try again")
-  #install.packages("devtools")
+  install.packages("devtools")
 }
 
 #pheatmap
 if(!library(pheatmap,logical.return=T)){
   warning("please install pheatmap and try again")
-  #install.packages("pheatmap")
+  install.packages("pheatmap")
 }
 
 #ggrepel
 if(!library(ggrepel,logical.return=T)){
   warning("please install pheatmap and try again")
-  #install.packages("ggrepel")
+  install.packages("ggrepel")
 }
 
 # download gsea
